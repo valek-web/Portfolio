a9e1394c1f7c261a0b602ccbc92ba07407e42af3
Valek <ziryanovvalentin@mail.ru>
2022-05-16 22:49:53 +0300

    contact

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ const close_menu = document.querySelector('.main-menu__close')
 const bg_menu = document.querySelector('.bg_menu')
 const main_menu = document.querySelector('.main-menu')
 const hamburger = document.querySelector('.hamburger')
+const menu_link = document.querySelectorAll('.main-menu__link')
 
 hamburger.addEventListener('click', () => {
   bg_menu.classList.toggle('bg_menu__active')
@@ -14,3 +15,11 @@ close_menu.addEventListener('click', () => {
   main_menu.classList.toggle('main-menu__active')
   hamburger.classList.toggle('hamburger-animation')
 })
+
+for (let i = 0; i < menu_link.length; i++) {
+  menu_link[i].addEventListener('click', () => {
+    bg_menu.classList.toggle('bg_menu__active')
+    main_menu.classList.toggle('main-menu__active')
+    hamburger.classList.toggle('hamburger-animation')
+  })
+}
