26bf48717295a82f96a137578b8db630a5d9b594
Valek <ziryanovvalentin@mail.ru>
2022-06-09 20:01:19 +0300

    descrProfile+

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ const bg_menu = document.querySelector('.bg_menu')
 const main_menu = document.querySelector('.main-menu')
 const hamburger = document.querySelector('.hamburger')
 const menu_link = document.querySelectorAll('.main-menu__link')
+const form = document.querySelector('.form')
 
 hamburger.addEventListener('click', () => {
   bg_menu.classList.toggle('bg_menu__active')
